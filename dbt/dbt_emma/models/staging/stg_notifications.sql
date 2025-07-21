4aa7dd3445b853a06d06a7226252243ba4e64811
denizozkaymak <denizgokalp.5@gmail.com>
2025-07-21 17:03:36 +0200

    Edited staging layer.

--- dbt/dbt_emma/models/staging/stg_notifications.sql
+++ dbt/dbt_emma/models/staging/stg_notifications.sql
@@ -4,8 +4,7 @@ select
         reason
         ,channel
         ,status
-        ,cast(user_id as int) as user_id
+        ,user_id
         --,timestamp_micros(created_date) as created_at
         ,timestamp_micros(cast(created_date / 1000 as int64)) as created_at
 from {{ source('raw', 'notifications') }}
-WHERE SAFE_CAST(user_id AS int) IS NOT NULL
--- dbt/dbt_emma/models/staging/stg_users.sql
+++ dbt/dbt_emma/models/staging/stg_users.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 
 select
-        cast(user_id as int) as user_id
+        user_id
         ,birth_year
         ,country
         ,timestamp_micros(cast(created_date / 1000 as int64)) as created_at
@@ -11,4 +11,3 @@ select
         ,attributes_notifications_marketing_email as marketing_email
         ,num_contacts
 from {{ source('raw', 'users') }}
-WHERE SAFE_CAST(user_id AS int) IS NOT NULL
